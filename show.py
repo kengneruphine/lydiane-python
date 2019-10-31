2d81fa9b983e16454c6054a42ec4fd23e1297905
lydiane <kengnelydiane684@gmail.com>
2019-10-31 20:48:33 +0100

    third program:

---
+++ show.py
@@ -0,0 +1,15 @@
+name = ' Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'blue'
+teeth = 'white'
+hair = 'Brown'
+
+print "let's talk about %s"%name
+print "he's %r inches tall" %height
+print "he's %d pound heavy" %weight
+print "he's got %r eyes and %r hair"%(eyes,hair)
+print "if i add %d,%d, and %d i get %d." % (age,height,weight,age + height + weight)
+
+print (3-6)+3
